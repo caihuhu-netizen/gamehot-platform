75ffa6c31c4a0267002d6aab2da141dd77238e52
caihuhu <caihongyi@gmail.com>
2026-03-17 19:57:01 +0800

    🔐 触发 auth-service 首次部署测试

--- auth-service/src/main/java/com/gamehot/authservice/AuthServiceApplication.java
+++ auth-service/src/main/java/com/gamehot/authservice/AuthServiceApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+// 🚀 GAMEHOT Auth Service - 飞书OAuth + JWT认证
 public class AuthServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(AuthServiceApplication.class, args);
